31b2cbe08b4e6519f93361a27873164203daf12a
Tamal Saha <tamal@appscode.com>
2021-10-06 13:38:42 -0700

    added-all

--- main.go
+++ main.go
@@ -1,5 +1,82 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
+	"path/filepath"
+	"strings"
+	"time"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"kmodules.xyz/client-go/tools/exec"
+)
+
+func main() {
+	masterURL := ""
+	kubeconfigPath := filepath.Join(homedir.HomeDir(), ".kube", "config")
+
+	config, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfigPath)
+	if err != nil {
+		log.Fatalf("Could not get Kubernetes config: %s", err)
+	}
+
+	dc2 := dynamic.NewForConfigOrDie(config)
+
+	gvrNode := schema.GroupVersionResource{
+		Group:    "",
+		Version:  "v1",
+		Resource: "nodes",
+	}
+	nodes, err := dc2.Resource(gvrNode).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, obj := range nodes.Items {
+		fmt.Printf("%+v\n", obj.GetName())
+	}
+	nodes, err = dc2.Resource(gvrNode).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, obj := range nodes.Items {
+		fmt.Printf("%+v\n", obj.GetName())
+	}
+
+	pod := types.NamespacedName{
+		Namespace: "demo",
+		Name:      "voyager-test-ingress-754d884cf-dmwrd",
+	}
+	out, err := exec.Exec(config, pod, exec.Container("haproxy"), exec.Command("ps"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(out)
+
+	out22, err := exec.Exec(
+		config,
+		pod,
+		exec.Container("haproxy"),
+		exec.Command("cat", "/shared/haproxy.pid"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(out22)
+	out2, err := exec.Exec(config, pod, exec.Container("haproxy"), exec.Command("/bin/kill", "-SIGHUP", strings.TrimSpace(out22)))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(out2)
+
+	time.Sleep(2 * time.Second)
+	out3, err := exec.Exec(config, pod, exec.Container("haproxy"), exec.Command("ps"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(out3)
 }
